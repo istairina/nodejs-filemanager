5903d0a3046b63d8623a498ef08aa929027eb351
Irina Migunova <ista.irina@gmail.com>
2023-06-19 15:45:06 +0300

    feat: farewell on .exit

--- src/index.js
+++ src/index.js
@@ -11,14 +11,22 @@ if (!username) {
     console.log('The username is set up to Anonymous, cause there\'s no flag --username or it\'s value is empty');
 }
 
+function farewell () {
+    process.stdout.write(`Thank you for using File Manager, ${username}, goodbye!\n`);
+    process.exit();
+}
+
 process.stdin.on("data", data => {
+    // console.log(String(data).trim())
+    if (String(data).trim() === '.exit') {
+        farewell();
+    }
     data = data.toString().toUpperCase()
-    process.stdout.write(data + "\n")
+    process.stdout.write(data)
 });
 
 process.on("SIGINT", () => {
-    process.stdout.write(`Thank you for using File Manager, ${username}, goodbye!\n`);
-    process.exit();
+    farewell();
 });
 
 
