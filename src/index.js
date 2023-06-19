1fcbb4510847099bf789a002f0bdfd450fbede7e
Irina Migunova <ista.irina@gmail.com>
2023-06-19 15:40:11 +0300

    feat: greetings with name and farewell on ctrl+c

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('ready');
+
+let username;
+process.argv.some(elem => {
+    if (elem.includes("--username")) {
+        username = elem.split('=')[1];
+        return true;
+    }
+});
+if (!username) {
+    username = 'Anonymous';
+    console.log('The username is set up to Anonymous, cause there\'s no flag --username or it\'s value is empty');
+}
+
+process.stdin.on("data", data => {
+    data = data.toString().toUpperCase()
+    process.stdout.write(data + "\n")
+});
+
+process.on("SIGINT", () => {
+    process.stdout.write(`Thank you for using File Manager, ${username}, goodbye!\n`);
+    process.exit();
+});
+
+
+
+console.log(`Welcome to the File Manager, ${username}!`);
