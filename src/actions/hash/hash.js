14163622066a284cf41047f98b883e9bcade6cd9
Irina Migunova <ista.irina@gmail.com>
2023-06-23 12:26:23 +0300

    feat: add hash command

---
+++ src/actions/hash/hash.js
@@ -0,0 +1,23 @@
+import { createHash } from "crypto";
+import fs from "fs/promises";
+import path from "path";
+import { currDir } from "../../index.js";
+import { INVALID, FAILED } from "../../constants/errors.js";
+
+export const hash = async (data) => {
+    if (data.length != 2) {
+        process.stdout.write(`${FAILED}\n`);
+        return;
+    };
+    const pathToFile = path.resolve(currDir, data[1]);
+    try {
+        const fileContent = await fs.readFile(pathToFile);
+        const hash = createHash("sha256").update(fileContent);
+        process.stdout.write(`${hash.digest("hex")}\n`);
+    }
+    catch (err) {
+        process.stdout.write(`${INVALID}\n`);
+    }
+};
+
+
