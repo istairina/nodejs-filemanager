a4f4795fe57a2b19891fe57fd502045622136039
Irina Migunova <ista.irina@gmail.com>
2023-06-23 10:15:24 +0300

    feat: command copy file into folder

--- src/actions/basic/cp.js
+++ src/actions/basic/cp.js
@@ -5,36 +5,35 @@ import fs from 'fs';
 
 export default async function cp(formatData) {
     if (formatData.length != 3) {
-        process.stdout.write(`${FAILED}\n`);
+        process.stdout.write(`${FAILED}\nnumber of args`);
         return;
     };
 
     const originalFile = path.resolve(currDir, formatData[1]);
-    let isOriginalFile;
+    const destination = path.resolve(currDir, formatData[2]);
     fs.stat(originalFile, (err, stats) => {
         if (err) {
             process.stdout.write(`${FAILED}\n`);
             return;
         } 
-        return stats.isFile();
-        // return stats.isFile();
-    }).then((data) => {
-        console.log(data);
+        if (!stats.isFile()) {
+            process.stdout.write(`${INVALID}\n`);
+            return;
+        }
+        fs.stat(destination, (err, stats) => {
+            if (err) {
+                process.stdout.write(`${FAILED}\n`);
+                return;
+            } 
+            if (!stats.isDirectory()) {
+                process.stdout.write(`${INVALID}\n`);
+                return;
+            }
+            const newFile = path.resolve(destination, path.basename(originalFile));
+            const readable = fs.createReadStream(originalFile);
+            const writable = fs.createWriteStream(newFile);
+            readable.pipe(writable);
+            console.log(`You are currently in ${currDir}`);
+        });
     });
-    
-    const newFile = path.resolve(currDir, formatData[2]);
-    const readable = fs.createReadStream(originalFile);
-    const writable = fs.createWriteStream(newFile);
-    readable.pipe(writable);
-        //    fs.stat(checkFile, (err) => {
-        //         if (!err) {
-        //             const fileStream = fs.createReadStream(checkFile);
-        //             fileStream.on('data', (chunk) => console.log(chunk));
-        //             fileStream.on('error', () => process.stdout.write(`${FAILED}: can't read the file\n`));
-        //             fileStream.on('end', () => process.stdout.write(`You are currently in ${currDir}\n`)); 
-        //         }
-        //         else if (err.code === 'ENOENT') {
-        //             process.stdout.write(`${INVALID}: there is no such file\n`);
-        //         }
-        //     });
 }
