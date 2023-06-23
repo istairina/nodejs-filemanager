c8bbc5231ef6d7079a8b7efaa4dc9e8802de180b
Irina Migunova <ista.irina@gmail.com>
2023-06-23 23:24:48 +0300

    feat: rewrite with process.cwd()

--- src/compress/compress.js
+++ src/compress/compress.js
@@ -1,7 +1,6 @@
 import { createReadStream, createWriteStream } from "fs";
 import { createBrotliCompress } from "zlib";
 import path from "path";
-import { currDir } from "../index.js";
 import { pipeline } from "stream";
 import { INVALID, FAILED } from "../constants/errors.js";
 
@@ -11,14 +10,14 @@ export const compress = async (data) => {
         process.stdout.write(`${FAILED}\n`);
         return;
     };
-    const source = createReadStream(path.resolve(currDir, data[1]));
-    const destination = createWriteStream(path.resolve(currDir, data[2]));
+    const source = createReadStream(path.resolve(process.cwd(), data[1]));
+    const destination = createWriteStream(path.resolve(process.cwd(), data[2]));
 
   pipeline(source, createBrotliCompress(), destination, (err) => {
     if (err) {
         process.stdout.write(`${FAILED}\n`);
         return;
     }
-    console.log(`You are currently in ${currDir}`);
+    console.log(`You are currently in ${process.cwd()}`);
   });
 };
