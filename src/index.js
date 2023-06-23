616dac9beca2b3961aa114da07b1943b3869c566
Irina Migunova <ista.irina@gmail.com>
2023-06-23 17:18:55 +0300

    feat create object with commands

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 
 import os from 'os';
-import { cd, ls, up } from './nwd/_nwd.js';
-import { cat, add, rn, cp, mv, rm } from './files/_files.js';
-import { osx } from './os/os.js';
-import { hash } from './hash/hash.js';
-import { compress, decompress } from './compress/_compress.js'
 import { username } from './constants/username.js';
 import { INVALID, FAILED } from './constants/errors.js';
 import { stdin as input, stdout as output } from 'node:process';
 import * as readline from 'node:readline/promises';
+import { commands } from './commands.js';
 
 const userHomeDir = os.homedir();
 export let currDir = userHomeDir;
@@ -17,60 +13,19 @@ export function setCurrDir(newDir) {
 }
 
 function farewell () {
-    process.stdout.write(`Thank you for using File Manager, ${username}, goodbye!\n`);
+    output.write(`Thank you for using File Manager, ${username}, goodbye!\n`);
     rl.close();
-}
+};
 
 const rl = readline.createInterface({ input, output });
 
 rl.on("line", (data) => {
     const formatData = String(data).trim().split(' ');
-        switch (formatData[0]) {
-            case '.exit':
-                farewell();
-                break;
-            case 'up':
-                up(formatData);
-                break;
-            case 'cd':
-                cd(formatData);
-                break;
-            case 'ls':
-                ls(formatData);
-                break;
-            case 'cat':
-                cat(formatData);
-                break;
-            case 'add':
-                add(formatData);
-                break;
-            case 'rn':
-                rn(formatData);
-                break;
-            case 'cp':
-                cp(formatData);
-                break;
-            case 'mv':
-                mv(formatData);
-                break;
-            case 'rm':
-                rm(formatData);
-                break;
-            case 'os':
-                osx(formatData);
-                break;
-            case 'hash':
-                hash(formatData);
-                break;
-            case 'compress':
-                compress(formatData);
-                break;
-            case 'decompress':
-                decompress(formatData);
-                break;
-            default: 
-                process.stdout.write(`${INVALID}\n`);
-        } 
+    if (commands[formatData[0]]) {
+        commands[formatData[0]](formatData);
+    } else {
+        console.log(`${INVALID}: wrong command`);
+    }
     }
 );
 
