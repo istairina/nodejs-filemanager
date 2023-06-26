72cc1cd9e11bc11f2cf0b11f3f37bd5e4834ecf4
Irina Migunova <ista.irina@gmail.com>
2023-06-26 16:24:37 +0300

    feat: color message

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,10 +1,10 @@
 export const failed = (message) => {
-  console.log(`Operation failed${message ? ": " + message : ""}`);
+  console.log('\x1b[31m%s\x1b[0m', `Operation failed${message ? ": " + message : ""}`);
 };
 
 export const invalid = (message) => {
-  console.log(`Invalid input${message ? ": " + message : ""}`);
+  console.log('\x1b[31m%s\x1b[0m', `Invalid input${message ? ": " + message : ""}`);
 };
 
 export const currentDir = () =>
-  console.log(`You are currently in ${process.cwd()}`);
+  console.log('\x1b[34m%s\x1b[0m', `You are currently in ${process.cwd()}`);
